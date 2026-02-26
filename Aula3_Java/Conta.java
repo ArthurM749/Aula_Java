e063270ed9cca29854c986b9595fe4496f5723d1
Arthur M <arthurmccomp@gmail.com>
2026-02-26 15:36:15 -0300

    Refactor Conta class to include methods with parameters

--- Aula3_Java/Conta.java
+++ Aula3_Java/Conta.java
@@ -2,28 +2,49 @@ package Aula3_Java;
 
 public class Conta {
 
-    String nomeBanco = "UBS Group";
-    int numeroConta = 198761234;
-    String agencia = "1337-77";
-    String nomeCliente = "Arthur Moura";
-    String tipoConta = "Corrente";
-    float saldo = 20000.00f;
-    float limite = 10000.00f;
+    String nomeBanco ;
+    int numeroConta ;
+    String agencia ;
+    String nomeCliente ;
+    String tipoConta ;
+    float saldo ;
+    float limite ;
 
-    void depositar(){
-
-    }
-
-    void sacar(){
-
-    }
-
-    void transferir(){
+    void passouLimite(float valor){
+        if (valor > limite){
+            System.out.println("O valor passou do limite permitido!");
+        }
 
     }
 
-    void pagar(){
-        
+    void depositar(float valor){
+        saldo += valor;
+
+        System.out.println("Valor depositado: " + valor + "\nTotal: "+ saldo);
+        System.out.println("......................");
+    }
+
+    void sacar(float valor){
+        saldo -= valor;
+        passouLimite(valor);
+        System.out.println("Valor sacado: " + valor + "\nTotal: "+ saldo);
+        System.out.println("......................");
+    }
+
+    void transferir(float valor){
+
+        saldo -= valor;
+        passouLimite(valor);
+        System.out.println("Valor trasferido: " + valor +"\nTotal: " + saldo);
+        System.out.println("......................");
+    }
+
+    void pagar(float valor){
+
+        saldo -= valor;
+        passouLimite(valor);
+        System.out.println("Valor pago: "+  valor+ "\nTotal: " + saldo);
+        System.out.println("......................");
     }
 
 }
